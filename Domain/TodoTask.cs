1c31b055f0133bb585d11b3cb86731c42f5078e8
Nemanja Milenkovic <nemanjamilenkovic@live.com>
2024-06-02 11:43:20 +0200

    Implement TodoTask class
    - Contains properties with private setters (so the class isn't immutable)
    - Contains some business rules as well

---
+++ Domain/TodoTask.cs
@@ -0,0 +1,32 @@
+﻿namespace Domain;
+
+
+public class TodoTask
+{
+    public string Title { get; private set; }
+    public DateTimeOffset DueDate { get; private set; }
+
+    public bool Finished { get; private set; }
+
+    public void Postpone(int days) {
+        if (Finished) {
+            throw new InvalidOperationException("You cannot postpone a finished task.");
+        }
+
+        if (days <= 0) {
+            throw new ArgumentOutOfRangeException(nameof(days), "You cannot postpone a task for less than 1 day.");
+        }
+
+        DueDate = DueDate.AddDays(days);
+    }
+
+    public void Complete() {
+        Finished = true;
+    }
+
+    public TodoTask(string title, DateTimeOffset dueDate)
+    {
+        Title = title;
+        DueDate = dueDate;
+    }
+}
